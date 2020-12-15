cace014ad9a1ac0d04f6452aa1793bfc063e34f6
Chase Manning <me@chasemanning.co.nz>
2020-12-15 20:07:56 +1300

    :rocket: Add Floor

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import Background from "./Background";
+import Floor from "./Floor";
 
 const StyledApp = styled.div`
   position: fixed;
@@ -14,6 +15,7 @@ const App = () => {
   return (
     <StyledApp>
       <Background topColor={"#145377"} bottomColor={"#6DCCC9"} />
+      <Floor color={"#16496E"} />
     </StyledApp>
   );
 };
