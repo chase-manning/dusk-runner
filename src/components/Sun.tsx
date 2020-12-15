a9e56d0ce89eee110d98e60c2388538046025f87
Chase Manning <me@chasemanning.co.nz>
2020-12-15 21:15:07 +1300

    :sparkles: Create Sun Component

---
+++ src/components/Sun.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledSun = styled.div`
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  width: ${(props: SunType) => props.diameter + "px"};
+  height: ${(props: SunType) => props.diameter + "px"};
+  border-radius: 50%;
+  background: ${(props: SunType) =>
+    "linear-gradient(" + props.highlight + ", " + props.shadow + ")"};
+  opacity: ${(props: SunType) => props.opacity};
+`;
+
+export type SunType = {
+  highlight: string;
+  shadow: string;
+  diameter: number;
+  opacity: number;
+};
+
+type Props = {
+  sun: SunType;
+};
+
+const Sun = (props: Props) => (
+  <StyledSun
+    highlight={props.sun.highlight}
+    shadow={props.sun.shadow}
+    diameter={props.sun.diameter}
+    opacity={props.sun.opacity}
+  />
+);
+
+export default Sun;
