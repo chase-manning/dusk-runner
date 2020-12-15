031341859f25f6d1a900b7e351cd2cd8d5cab3be
Chase Manning <me@chasemanning.co.nz>
2020-12-15 19:35:15 +1300

    :rocket: Add Background

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,21 @@
 import React from "react";
 import styled from "styled-components";
+import Background from "./Background";
 
 const StyledApp = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
   width: 100%;
   height: 100%;
 `;
 
 const App = () => {
-  return <StyledApp></StyledApp>;
+  return (
+    <StyledApp>
+      <Background topColor={"#145377"} bottomColor={"#6DCCC9"} />
+    </StyledApp>
+  );
 };
 
 export default App;
