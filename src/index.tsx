be480dac96b0084ec11be1ac9c8c2cb6712d1631
Chase Manning <me@chasemanning.co.nz>
2020-12-16 13:51:26 +1300

    :rocket: Add Global Styles to Index

--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,31 @@ import App from "./components/App";
 import { store } from "./store/store";
 import { Provider } from "react-redux";
 import * as serviceWorker from "./serviceWorker";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  :root {
+    --test: red;
+    --test2: blue;
+  }
+
+  * {
+      box-sizing: border-box;
+      margin: 0;
+      padding: 0;
+      font-family: monospace;
+      font-size: 12px;
+  }
+  button {
+    background: none; border: none;
+      outline: none;
+  }
+`;
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
+      <GlobalStyle />
       <App />
     </Provider>
   </React.StrictMode>,
