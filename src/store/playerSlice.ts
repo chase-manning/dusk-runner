54456da58599b5c39e2dbc4afbfa8a60ef998bbe
Chase Manning <me@chasemanning.co.nz>
2020-12-16 18:11:09 +1300

    :sparkles: Add Player Hight to State

--- src/store/playerSlice.ts
+++ src/store/playerSlice.ts
@@ -1,4 +1,5 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { RootState } from "./store";
 
 export enum PlayerState {
   RUNNING,
@@ -7,10 +8,12 @@ export enum PlayerState {
 
 interface playerState {
   state: PlayerState;
+  height: number;
 }
 
 const initialState: playerState = {
   state: PlayerState.RUNNING,
+  height: 0,
 };
 
 export const playerSlice = createSlice({
@@ -23,11 +26,15 @@ export const playerSlice = createSlice({
     run: (state) => {
       state.state = PlayerState.RUNNING;
     },
-    // incrementByAmount: (state, action: PayloadAction<number>) => {
-    //   state.value += action.payload;
-    // },
+    setHeight: (state, action: PayloadAction<number>) => {
+      state.height = action.payload;
+    },
   },
 });
 
-export const { jump, run } = playerSlice.actions;
+export const { jump, run, setHeight } = playerSlice.actions;
+
+export const selectState = (state: RootState) => state.player.state;
+export const selectHeight = (state: RootState) => state.player.height;
+
 export default playerSlice.reducer;
