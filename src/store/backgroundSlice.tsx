7e41053e32e50a51232ddc9de005ba586ecaeef1
Chase Manning <me@chasemanning.co.nz>
2020-12-16 19:38:56 +1300

    :sparkles: Create Background Slice

---
+++ src/store/backgroundSlice.tsx
@@ -0,0 +1,27 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import Background from "../components/Background";
+import { RootState } from "./store";
+
+interface backgroundState {
+  movement: number;
+}
+
+const initialState: backgroundState = {
+  movement: 0,
+};
+
+export const backgroundSlice = createSlice({
+  name: "background",
+  initialState,
+  reducers: {
+    setMovement: (state, action: PayloadAction<number>) => {
+      state.movement = action.payload;
+    },
+  },
+});
+
+export const { setMovement } = backgroundSlice.actions;
+
+export const selectMovement = (state: RootState) => state.background.movement;
+
+export default backgroundSlice.reducer;
