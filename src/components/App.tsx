27710a54a3a32729be3c467f867e9f1371fe6975
Chase Manning <me@chasemanning.co.nz>
2020-12-16 13:51:59 +1300

    :rocket: Add Mountain

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import Mountain from "./Mountain";
 import Background from "./Background";
 import Floor from "./Floor";
 import Stars from "./Stars";
@@ -13,12 +14,22 @@ const StyledApp = styled.div`
   height: 100%;
 `;
 
+const StyledMountain = styled.img`
+  position: absolute;
+  width: 50%;
+`;
+
 const App = () => {
   return (
     <StyledApp>
       <Background topColor={"#145377"} bottomColor={"#6DCCC9"} />
       <Suns highlight={"#EDF5ED"} shadow={"#EDF5ED00"} count={7} />
       <Stars />
+      <Mountain
+        topColor={"#266C88"}
+        bottomColor={"#6CC7C6"}
+        height={window.innerHeight / 2}
+      />
       <Floor color={"#16496E"} />
     </StyledApp>
   );
