d53d197e48ba7d512dc8f06a150ef198278f0f16
Chase Manning <me@chasemanning.co.nz>
2020-12-16 18:30:30 +1300

    :sparkles: Add Player Speed

--- src/store/playerSlice.ts
+++ src/store/playerSlice.ts
@@ -9,11 +9,13 @@ export enum PlayerState {
 interface playerState {
   state: PlayerState;
   height: number;
+  speed: number;
 }
 
 const initialState: playerState = {
   state: PlayerState.RUNNING,
   height: 0,
+  speed: 10,
 };
 
 export const playerSlice = createSlice({
@@ -29,12 +31,16 @@ export const playerSlice = createSlice({
     setHeight: (state, action: PayloadAction<number>) => {
       state.height = action.payload;
     },
+    setSpeed: (state, action: PayloadAction<number>) => {
+      state.speed = action.payload;
+    },
   },
 });
 
-export const { jump, run, setHeight } = playerSlice.actions;
+export const { jump, run, setHeight, setSpeed } = playerSlice.actions;
 
 export const selectState = (state: RootState) => state.player.state;
 export const selectHeight = (state: RootState) => state.player.height;
+export const selectSpeed = (state: RootState) => state.player.speed;
 
 export default playerSlice.reducer;
