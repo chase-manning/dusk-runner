4405c68c834a49c9fbdb1fbfc618d7a98ec65063
Chase Manning <me@chasemanning.co.nz>
2020-12-16 19:39:09 +1300

    :sparkles: Add Background Slice to Store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,11 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
 import playerReducer from "./playerSlice";
+import backgroundReducer from "./backgroundSlice";
 
 export const store = configureStore({
   reducer: {
     player: playerReducer,
+    background: backgroundReducer,
   },
 });
 
