7084de96c0adfc077d394dd5fe8ebbc9a9d640d1
Chase Manning <me@chasemanning.co.nz>
2020-12-17 18:53:12 +1300

    :sparkles: Add Foreground to Store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,11 +1,13 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
 import playerReducer from "./playerSlice";
 import backgroundReducer from "./backgroundSlice";
+import foregroundReduce from "./foregroundSlice";
 
 export const store = configureStore({
   reducer: {
     player: playerReducer,
     background: backgroundReducer,
+    foreground: foregroundReduce,
   },
 });
 
