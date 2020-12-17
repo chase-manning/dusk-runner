04d483db95f62b9b06a3fb3760cdc09923faa4fa
Chase Manning <me@chasemanning.co.nz>
2020-12-18 03:40:41 +1300

    :sparkles: Create Zones

---
+++ src/config/zones.ts
@@ -0,0 +1,20 @@
+import { Zone } from "../store/backgroundSlice";
+
+const zones: Zone[] = [
+  {
+    backgroundTopColor: "#145377",
+    backgroundBottomColor: "#90E8E4",
+    sunHighlightColor: "#EDF5ED",
+    sunShadowColor: "#EDF5ED00",
+    floorColor: "#16496e",
+  },
+  {
+    backgroundTopColor: "#90C0BC",
+    backgroundBottomColor: "#EE8BB0",
+    sunHighlightColor: "#F7F5EF",
+    sunShadowColor: "#EEA6BC",
+    floorColor: "#04222C",
+  },
+];
+
+export default zones;
