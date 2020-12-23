ece4988e741c0034ba3e9b2fae17e025e1becd63
Chase Manning <me@chasemanning.co.nz>
2020-12-23 19:17:03 +1300

    :rocket: Add Locked Status to Unlocked Stages

--- src/components/StageOption.tsx
+++ src/components/StageOption.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import styled from "styled-components";
-import { GameState, selectGameState, setGameState } from "../store/gameSlice";
+import {
+  GameState,
+  selectCompletedStages,
+  selectGameState,
+  setGameState,
+} from "../store/gameSlice";
 import { resetBackground, setZone } from "../store/backgroundSlice";
 import { Zone } from "../config/zones";
 import { run, setSpeed } from "../store/playerSlice";
@@ -28,6 +33,15 @@ const ZoneImage = styled.img`
   height: 100%;
 `;
 
+const Disabled = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+`;
+
 type Props = {
   zone: Zone;
 };
@@ -35,6 +49,7 @@ type Props = {
 const StageOption = (props: Props) => {
   const dispatch = useDispatch();
   const gameState = useSelector(selectGameState);
+  const completedStages = useSelector(selectCompletedStages);
 
   if (gameState !== GameState.MENU) return null;
 
@@ -49,6 +64,7 @@ const StageOption = (props: Props) => {
       }}
     >
       <ZoneImage src={props.zone.asset} />
+      {completedStages < props.zone.stage - 1 && <Disabled>Locked</Disabled>}
     </StyledStageOption>
   );
 };
