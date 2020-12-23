a3766325bcab1f7d7f792558626ee35c6a5378cd
Chase Manning <me@chasemanning.co.nz>
2020-12-23 17:58:28 +1300

    :rocket: Add Zone Selections

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { useSelector } from "react-redux";
 import styled from "styled-components";
 import { GameState, selectGameState } from "../store/gameSlice";
+import zone01 from "../assets/zone-01.png";
+import zone02 from "../assets/zone-02.png";
 
 const StyledMenu = styled.div`
   position: fixed;
@@ -15,9 +17,26 @@ const StyledMenu = styled.div`
 `;
 
 const StageOption = styled.div`
-  height: 70%;
+  height: 60%;
   background-color: green;
   width: 200px;
+  overflow: hidden;
+  border-radius: 40px;
+  border: white solid 2px;
+  box-shadow: 10px 10px 10px black;
+  margin-right: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+`;
+
+const ZoneImage = styled.img`
+  position: absolute;
+  top: 0;
+  left: 50%;
+  transform: translateX(-50%);
+  height: 100%;
 `;
 
 const Menu = () => {
@@ -27,8 +46,12 @@ const Menu = () => {
 
   return (
     <StyledMenu>
-      <StageOption>test</StageOption>
-      <StageOption>test</StageOption>
+      <StageOption>
+        <ZoneImage src={zone01} />
+      </StageOption>
+      <StageOption>
+        <ZoneImage src={zone02} />
+      </StageOption>
     </StyledMenu>
   );
 };
