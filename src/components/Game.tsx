05e174545390a4c1633559f5bbc71d66cced455a
Chase Manning <me@chasemanning.co.nz>
2020-12-23 17:58:02 +1300

    :lipstick: Add Game Blur in Menu

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,5 +1,7 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import styled from "styled-components";
+import { GameState, selectGameState } from "../store/gameSlice";
 import Background from "./Background";
 import BackgroundAssets from "./BackgroundAssets";
 import EventHandler from "./EventHandler";
@@ -10,17 +12,27 @@ import Player from "./Player";
 import Stars from "./Stars";
 import Suns from "./Suns";
 
+type StyledGameProps = {
+  blur: boolean;
+};
+
 const StyledGame = styled.div`
   width: 100%;
   height: 100%;
   position: fixed;
   top: 0;
   height: 100;
+  filter: ${(props: StyledGameProps) =>
+    props.blur ? "blur(10px)" : "blur(0px)"};
+  transform: ${(props: StyledGameProps) =>
+    props.blur ? "scale(1.2)" : "scale(1)"};
 `;
 
 const Game = () => {
+  const gameState = useSelector(selectGameState);
+
   return (
-    <StyledGame>
+    <StyledGame blur={gameState === GameState.MENU}>
       <Background />
       <Suns />
       <Stars />
