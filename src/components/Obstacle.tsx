9d0358d3302c5e99c11f6de6c23a527a3679aa15
Chase Manning <me@chasemanning.co.nz>
2020-12-23 17:37:14 +1300

    :zap: Improve Performance of Obstacle

--- src/components/Obstacle.tsx
+++ src/components/Obstacle.tsx
@@ -1,13 +1,8 @@
 import React from "react";
 import styled from "styled-components";
 
-type StyledObstacleProps = {
-  left: number;
-};
-
 const StyledObstacle = styled.div`
   position: fixed;
-  left: ${(props: StyledObstacleProps) => props.left + "px"};
   bottom: 5%;
   width: 20px;
   height: 40px;
@@ -23,7 +18,9 @@ type Props = {
 };
 //TODO Change this to not use global styles for animations
 const Obstacle = (props: Props) => {
-  return <StyledObstacle left={props.obstacle.left}></StyledObstacle>;
+  return (
+    <StyledObstacle style={{ left: props.obstacle.left }}></StyledObstacle>
+  );
 };
 
 export default Obstacle;
