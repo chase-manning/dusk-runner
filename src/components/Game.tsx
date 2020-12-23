6f8cf22d7f2a1ebba2405bb8edbf3ea827ff6ab0
Chase Manning <me@chasemanning.co.nz>
2020-12-23 17:44:01 +1300

    :lipstick: Always Show Game

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,7 +1,5 @@
 import React from "react";
-import { useSelector } from "react-redux";
 import styled from "styled-components";
-import { GameState, selectGameState } from "../store/gameSlice";
 import Background from "./Background";
 import BackgroundAssets from "./BackgroundAssets";
 import EventHandler from "./EventHandler";
@@ -21,10 +19,6 @@ const StyledGame = styled.div`
 `;
 
 const Game = () => {
-  const gameState = useSelector(selectGameState);
-
-  if (gameState === GameState.MENU) return null;
-
   return (
     <StyledGame>
       <Background />
