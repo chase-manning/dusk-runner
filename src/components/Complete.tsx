6b0f97709fe017a0781ac099a139bdaff2e6a892
Chase Manning <me@chasemanning.co.nz>
2020-12-23 18:52:48 +1300

    :sparkles: Create Complete Component

---
+++ src/components/Complete.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import styled from "styled-components";
+import { PlayerState, selectState } from "../store/playerSlice";
+import Player from "./Player";
+
+const StyledComplete = styled.div`
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Text = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+type Props = {
+  complete: boolean;
+};
+
+const Header = styled.div`
+  font-size: 20rem;
+  color: var(--foreground);
+  margin-bottom: 20px;
+  opacity: ${(props: Props) => (props.complete ? "1" : "0")};
+  transition: 2s opacity 2s;
+`;
+
+const SubHeader = styled.div`
+  font-size: 10rem;
+  color: var(--foreground);
+  opacity: ${(props: Props) => (props.complete ? "1" : "0")};
+  transition: 4s opacity 2s;
+`;
+
+const Complete = () => {
+  const state = useSelector(selectState);
+
+  return (
+    <StyledComplete>
+      <Text>
+        <Header complete={state === PlayerState.COMPLETE}>
+          Stage Complete
+        </Header>
+        <SubHeader complete={state === PlayerState.COMPLETE}>
+          Tap to continue
+        </SubHeader>
+      </Text>
+    </StyledComplete>
+  );
+};
+
+export default Complete;
